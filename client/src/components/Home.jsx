8bcdb8d192d9fe311dd1c08defdf7a3624bfa4bf
Paras Sehwag <parassehwag7777@gmail.com>
2024-02-10 17:24:40 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,86 +1,92 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 import axios from "axios";
-import {useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function Home() {
-
-  const [notes, setNotes] = useState([{title:'', content:''}]);
-  const {username} = useParams();
-    useEffect(()=>{
-          async function fetchNotes(){
-            try{
-              let response = await axios.get(`https://google-keep-kappa-gray.vercel.app/getNotes`,{
-                params: {username:username}
-              })
-              if(response.status===200){
-                setNotes(response.data);
-              }
-              else{
-                console.log('error');
-              }
-            }
-            catch(error){
-              console.log("Error",error)
-            }
-          }
-          fetchNotes();
-    },[username,notes])
-
-    async function addNote(newNote) {
-      try{
-        console.log(username);
-        let response = await axios.post(`https://google-keep-kappa-gray.vercel.app/addNote`,{newNote,username});
-        if(response.status===200){
-          console.log("note uploaded successfully")
-        }
-      }
-      catch(error){
-        console.log("Error during uploading note",error)
-      }
-    }
-  
-    async function deleteNote(id) {
-      setNotes(prevNotes => {
-        return prevNotes.filter((noteItem, index) => {
-          return index !== id;
-        });
-      });
-
-      try{
-        let response = await axios.delete(`https://google-keep-kappa-gray.vercel.app/deleteNote`,{
-          params:{username:username,id:id}
+  var header = {
+    'Access-Control-Allow-Origin': '*'
+  };
+  const [notes, setNotes] = useState([{ title: '', content: '' }]);
+  const { username } = useParams();
+  useEffect(() => {
+    async function fetchNotes() {
+      try {
+        let response = await axios.get(`https://google-keep-kappa-gray.vercel.app/getNotes`, {
+          headers: header,
+          params: { username: username }
         })
-        if(response.status===200){
-          console.log("Deleted Successfully");
+        if (response.status === 200) {
+          setNotes(response.data);
+        }
+        else {
+          console.log('error');
         }
       }
-      catch(error){
-          console.log('error',error)
+      catch (error) {
+        console.log("Error", error)
       }
     }
-  
-    return (
-      <div>
-        <Header />
-        <CreateArea onAdd={addNote} />
-        {notes.map((noteItem, index) => {
-          return (
-            <Note
-              key={index}
-              id={noteItem._id}
-              title={noteItem.title}
-              content={noteItem.content}
-              onDelete={(e)=>{deleteNote(e)}}
-            />
-          );
-        })}
-        <Footer />
-      </div>
-    );
+    fetchNotes();
+  }, [username, notes])
+
+  async function addNote(newNote) {
+    try {
+      console.log(username);
+      let response = await axios.post(`https://google-keep-kappa-gray.vercel.app/addNote`, { newNote, username }, {
+        headers: header
+      });
+      if (response.status === 200) {
+        console.log("note uploaded successfully")
+      }
+    }
+    catch (error) {
+      console.log("Error during uploading note", error)
+    }
   }
-  
-  export default Home;
+
+  async function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+
+    try {
+      let response = await axios.delete(`https://google-keep-kappa-gray.vercel.app/deleteNote`, {
+        headers:header,
+        params: { username: username, id: id }
+      })
+      if (response.status === 200) {
+        console.log("Deleted Successfully");
+      }
+    }
+    catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={noteItem._id}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={(e) => { deleteNote(e) }}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
