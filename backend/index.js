7ff1bcb8cc1f018bef70e41ecc8f361a292fc118
Paras Sehwag <parassehwag7777@gmail.com>
2024-02-10 17:42:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import session from "express-session";
-import bodyParser from "body-parser";
-import cors from "cors";
-import Router from './route.js';
-import Connection from './DbConnection/Connection.js';
 const app = express();
-
+import cors from "cors";
 app.use(cors({
   origin: '*',
   methods: ["POST", "GET", "DELETE"],
   credentials: true
 }));
+
+import session from "express-session";
+import bodyParser from "body-parser";
+import Router from './route.js';
+import Connection from './DbConnection/Connection.js';
+
 app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(session({
